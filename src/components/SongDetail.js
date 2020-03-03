f2cce8c13a91409628bda5c43dcfeb94e801f928
Jen Merritt <jenjomez@gmail.com>
2020-03-03 16:25:43 +0000

    mvp done.

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const SongDetail = props => {
   if(!props.song) return null;
-  return <h3>{props.song.title.label}</h3>
+  return (
+    <article>
+      <h3>{props.song.title.label}</h3>
+      <a href={props.song.id.label}><p>Buy Song on iTunes</p></a>
+      <p>Genre: {props.song.category.attributes.term}</p>
+      <p>Release Date: {props.song['im:releaseDate'].attributes.label}</p>
+      <p>Price: {props.song['im:price'].label}</p>
+    </article>
+
+  )
 
 }
 
